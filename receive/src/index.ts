428ea5b30c78c1580d7958b2aff3fdd8afb8eb57
qiu-alex <alex.qiu2012@gmail.com>
2022-12-11 20:40:10 -0500

    cleaning up code

--- receive/src/index.ts
+++ receive/src/index.ts
@@ -1,28 +1,14 @@
-/* eslint-disable space-before-blocks */
-/* eslint-disable prefer-template */
-/* eslint-disable no-multiple-empty-lines */
-/* eslint-disable padded-blocks */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable semi */
-/* eslint-disable spaced-comment */
-import dotenv from 'dotenv'
-import bodyParser from 'body-parser'
-import express, { Request, Response } from 'express'
-import amqp from 'amqplib'
+import bodyParser from 'body-parser';
+import express, { Request, Response } from 'express';
+import amqp from 'amqplib';
 
-//import createMQConsumer from './consumer'
-
-dotenv.config()
-
-const PORT = 3001
-const AMQP_URL = 'amqps://gluuahtn:nRdjO88xorXpUWBp1dhiNkWmhoCdN38J@moose.rmq.cloudamqp.com/gluuahtn'
-const QUEUE_NAME = 'test'
+const PORT = 3001;
+const AMQP_URL = 'amqps://gluuahtn:nRdjO88xorXpUWBp1dhiNkWmhoCdN38J@moose.rmq.cloudamqp.com/gluuahtn';
+const QUEUE_NAME = 'test';
 
 const app = express();
 app.use(bodyParser.json());
-//const consumer = createMQConsumer(AMQP_URL, QUEUE_NAME)
 
-//consumer()
 async function connect() {
   const conn = await amqp.connect(AMQP_URL);
 
@@ -30,6 +16,7 @@ async function connect() {
 
   await channel.assertQueue(QUEUE_NAME);
 
+  //consume messages and log them
   channel.consume(QUEUE_NAME, (msg) => {
     if (msg){
       console.log(msg.content.toString());
